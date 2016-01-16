f3f8f795365ed7430bdced67d6b269e7396de55c
fujic <fujic@fujiikousei-no-MacBook-Pro.local>
2016-01-16 20:19:56 +0900

    Modify js files.

--- jsLib/Output.js
+++ jsLib/Output.js
@@ -2,18 +2,23 @@
  文字列をブラウザの機能を利用して表示する。
  
  */
-//現在の表示している。HTMLに表示する
-  //末尾に書き込む
-  document.write('<p style="Color:Blue">Hello World!</p>');
+  //現在の表示している。HTMLに表示する
+    //<body>末尾に書き込む
+    document.write('<p style="Color:Blue">Hello World!</p><div id="p-last"></div>');
   //DOMツリーを利用した表示div配列を利用
-  var divNodes = document.getElementsByTagName("div");
-  //console.log(divNodes);
-  //div配列の最初のノードに対してHello World!!を表示させる。
-  divNodes[0].firstChild.nodeValue = "Hello World!!";
-//アラートダイアログを表示する。
-  alert("Hello Dialog!!"); //シングル・ダブルコーテーションで囲む
-//ブラウザのデバッグコンソールを利用する。
-  console.log('Hello Console.log!');
+    //getElementsByTagNameでDOMを取得
+    var divNodes = document.getElementsByTagName("div");//{{{
+      //div配列の最初のノードに対してHello World!!を表示させる。
+    divNodes[0].firstChild.nodeValue = "Hello World!!";//}}}
+    //getElementsByClassNameでDOMを取得し表示する。
+    var divNodes = document.getElementsByClassName("df-value");//{{{
+      //class="df-value"配列の2番目のノードに対してGet DOM by Classを表示させる。
+    divNodes[1].firstChild.nodeValue = "Get by ClassName!";//}}}
+    //getElementsByIdでDOMを取得
+    var divNode = document.getElementById("df-N02");//{{{
+      //class="df-value"配列の2番目のノードに対してGet DOM by Classを表示させる。
+    divNode.firstChild.nodeValue = "Div Number 03!!";//}}}
+    var lstElm = document.getElementById("p-last");
+    lstElm.innerHTML = "<p>bodyタグの末尾に追加されます。</p>";
 
-//Console.logでデバッグが終了したことを表示する。
-console.log('----------  End of Script  ----------');
+
