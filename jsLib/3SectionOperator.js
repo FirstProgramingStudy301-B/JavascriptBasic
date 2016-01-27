d964c60f0d9e73a151c9f4141e819eec5e951e06
fujic <fujic@fujiikousei-no-MacBook-Pro.local>
2016-01-27 14:57:13 +0900

    01.27 del

--- jsLib/3SectionOperator.js
+++ jsLib/3SectionOperator.js
@@ -14,4 +14,51 @@ var xx = (X) ? '真': '偽';
 console.log('xx' + xx);
 var xxx = (X);
 console.log(xxx);
-Script_Abort();
+//Script_Abort(); //存在しない関数を呼び出してプログラムを停止
+//JavaScriptの値の評価
+/*
+ * {}   オブジェクト  true
+ * "hoge"   文字列  true
+ * "  文字  false
+ * 1  数値  true
+ * -1   数値  true
+ *  0   数値  false
+ *  []  配列  true
+ *  true  真偽値  true
+ *  false   真偽値  false
+ *  undefined   undefined   false
+ *  null  null  false"
+ */
+var valAry = [{},"hoge","",1,0,[],true,false,undefined,null];
+for(var i=0; valAry.length > i ;i++){
+  var val = JSON.stringify(valAry[i]);
+  console.log("評価の対象" + val + ": " + (valAry[i] ? true : false));
+}
+//論理演算子を使用した代入
+/*
+ * 【expr1 && expr2】
+ * ・expr1 を false と見ることができる場合は、expr1 を返します。
+ * ・そうでない場合は、expr2 を返します。
+ * ・したがって、真偽値と共に使われた場合、 演算対象の両方が true ならば、&& は、true を返し、
+ * 　そうでなければ、false を返します。
+ *
+ * 【expr1 || expr2】
+ * ・expr1 を true と見ることができる場合は、expr1 を返します。
+ * ・そうでない場合は、expr2 を返します。
+ * ・したがって、真偽値と共に使われた場合、 演算対象のどちらかが true ならば、|| は、true を返し、
+ * 　両方とも false の場合は、false を返します。
+ */
+
+var unVriant;
+var res = unVriant || "変数unVriableが未定義のため本文書が代入";
+console.log(res);
+
+var obj01 = {id:10,name:"hogera"};
+var obj03 = {};
+var obj02;
+function getId(obj){
+  return obj && obj.id;
+}
+console.log(getId(obj01));
+console.log(getId(obj02));
+console.log(getId(obj03));
--- jsLib/Output.js
+++ jsLib/Output.js
@@ -4,21 +4,62 @@
  */
   //現在の表示している。HTMLに表示する
     //<body>末尾に書き込む
+    //alert("HTMLの末尾に要素を追加します。");
     document.write('<p style="Color:Blue">Hello World!</p><div id="p-last"></div>');
   //DOMツリーを利用した表示div配列を利用
+    //alert("最初のDiv要素を変更します。");
     //getElementsByTagNameでDOMを取得
     var divNodes = document.getElementsByTagName("div");//{{{
       //div配列の最初のノードに対してHello World!!を表示させる。
     divNodes[0].firstChild.nodeValue = "Hello World!!";//}}}
+    //alert("２番目の要素を変更します。");
     //getElementsByClassNameでDOMを取得し表示する。
     var divNodes = document.getElementsByClassName("df-value");//{{{
       //class="df-value"配列の2番目のノードに対してGet DOM by Classを表示させる。
     divNodes[1].firstChild.nodeValue = "Get by ClassName!";//}}}
+    //alert("３番目の要素を変更します。");
     //getElementsByIdでDOMを取得
     var divNode = document.getElementById("df-N02");//{{{
       //class="df-value"配列の2番目のノードに対してGet DOM by Classを表示させる。
     divNode.firstChild.nodeValue = "Div Number 03!!";//}}}
+    //innerhtmlの使用例
+    alert("３番目の要素を変更します。");
     var lstElm = document.getElementById("p-last");
     lstElm.innerHTML = "<p>bodyタグの末尾に追加されます。</p>";
 
 
+  //複数のElementを追加するテクニック
+
+    //document.createDocumentFragment();
+    // サンプル1: ul要素にli要素をまとめて追加（高速）
+    //var ul = document.querySelector(‘#output’),
+    //    fragment = document.createDocumentFragment();
+    //
+    //for ( var i = 0; i < data.length; i++ ) {
+    //  var li = document.createElement(‘li’);
+    //  li.textContent = data[i];
+    //
+    //  // いったんDocumentFragmentに追加する
+    //  fragment.appendChild(li);
+    //}
+    //
+    //// 最後にDocumentFragmentをul要素に追加する
+    //ul.appendChild(fragment);
+    //template document.createElemnt() を保存しておく
+
+    //n = document.querySelector(‘.results’),
+    //    clone = origin.cloneNode(true);
+    //
+    //// コピーした要素を更新する
+    //var elements = clone.querySelectorAll(‘p’);
+    //elements[0].textContent = ‘結果a’;
+    //elements[1].textContent = ‘結果b’;
+    //elements[2].textContent = ‘結果c’;
+
+    //// 元の要素とコピーした要素を入れ替える
+    //origin.parentNode.replaceChild(clone, origin);ii
+
+    //複数スタイルの変更
+    // 複数のスタイルの書き換え（高速）
+    // style属性で一度にすべて指定する
+    //element.setAttribute(‘style’, ‘background: gray; border: 1px solid gray;’);
